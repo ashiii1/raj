67c268f46e1fa9f8f607b21f8fc3e92d8e600c0f
ashiii1 <aashishaik526@gmail.com>
2024-05-09 22:24:19 +0530

    .

--- src/componants/Footer.js
+++ src/componants/Footer.js
@@ -2,93 +2,38 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="max-w-[1520px] m-auto px-4 py-2 bg-[#24262b]">
-      <div className="py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
-        <div className="col-span-1">
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id odit
-            ullam iste repellat consequatur libero reiciendis, blanditiis
-            accusantium.
-          </p>
-          <div className="mt-6">
-            <form className="flex items-center">
-              <input
-                type="email"
-                className="bg-gray-800 text-gray-300 rounded-l px-4 py-2 focus:outline-none"
-                placeholder="Email address"
-              />
-              <button className="bg-orange-500 text-white px-4 py-2 rounded-r">
-                Subscribe
-              </button>
-            </form>
-          </div>
-        </div>
-
-        <div className="lg:col-span-2 flex justify-between mt-6">
-          <div className="flex flex-col">
-            <h6 className="font-medium text-[#9b9b9b] underline">Orders and support</h6>
-            <ul>
-              <li>Contact us</li>
-              <li>FAQs</li>
-              <li>Shipping</li>
-              <li>Returns</li>
-              <li>Order history</li>
-              <li>Check gift card balance</li>
-              <li>Terms and conditions</li>
-            </ul>
-          </div>
-          <div className="flex flex-col">
-            <h6 className="font-medium text-[#9b9b9b] underline">Services</h6>
-            <ul>
-              <li>Live assistance</li>
-              <li>Corporate gifts</li>
-              <li>Facial Appointments</li>
-              <li>Click and Collect</li>
-              <li>Video consultation</li>
-              <li>Location preferences</li>
-            </ul>
-          </div>
-          <div className="flex flex-col">
-            <h6 className="font-medium text-[#9b9b9b] underline">Shipping:</h6>
-            <p>Hong Kong, SAR</p>
-            <h6 className="font-medium text-[#9b9b9b] underline">Language:</h6>
-            <ul>
-              <li>繁體中文</li>
-              <li>English</li>
-            </ul>
-          </div>
+    <footer className="bg-gray-900 text-white">
+      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
+        <h1
+          className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold
+         md:w-2/5"
+        >
+          <span className="text-teal-400">Free</span> until you're ready to
+          launch
+        </h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter Your ph.no"
+            className="text-gray-800
+           sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none"
+          />
+          <button
+            className="bg-teal-400 hover:bg-teal-500 duration-300 px-5 py-2.5 font-[Poppins]
+           rounded-md text-white md:w-auto w-full"
+          >
+            Request Code
+          </button>
         </div>
       </div>
-      <div className="border-t border-gray-500 mt-6 pt-4">
-        <div className="flex justify-between">
-          <div className="flex flex-col">
-            <h6 className="font-medium text-[#9b9b9b] underline">Sustainability</h6>
-            <p>All Aesop products are vegan, and we do not test our formulations or ingredients on animals. We are Leaping Bunny approved and a Certified B Corporation. Learn more</p>
-          </div>
-          <div className="flex flex-col">
-            <h6 className="font-medium text-[#9b9b9b] underline">About</h6>
-            <ul>
-              <li>Our story</li>
-              <li>Foundation</li>
-              <li>Careers</li>
-              <li>Privacy policy</li>
-              <li>Accessibility</li>
-              <li>Cookie Policy</li>
-            </ul>
-          </div>
-          <div className="flex flex-col">
-            <h6 className="font-medium text-[#9b9b9b] underline">Social media</h6>
-            <ul>
-              <li>Instagram</li>
-              <li>Twitter</li>
-              <li>LinkedIn</li>
-              <li>WeChat</li>
-              <li>Weibo</li>
-            </ul>
-          </div>
-        </div>
+      <div
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10
+      text-center pt-2 text-gray-400 text-sm pb-8"
+      >
+        <span>© 2020 Appy. All rights reserved.</span>
+        <span>Terms · Privacy Policy</span>
       </div>
-    </div>
+    </footer>
   );
 };
 
